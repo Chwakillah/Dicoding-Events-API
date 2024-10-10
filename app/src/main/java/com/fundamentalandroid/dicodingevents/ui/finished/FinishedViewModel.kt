e4afe479053f6095c350bca167e5341d6f5b7ff3
Chwakillah <syakillahnachwa081@gmail.com>
2024-10-11 00:00:22 +0700

    init: start project

---
+++ app/src/main/java/com/fundamentalandroid/dicodingevents/ui/finished/FinishedViewModel.kt
@@ -0,0 +1,13 @@
+package com.fundamentalandroid.dicodingevents.ui.finished
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class FinishedViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
---
+++ app/src/main/java/com/fundamentalandroid/dicodingevents/ui/upcoming/UpcomingViewModel.kt
@@ -0,0 +1,13 @@
+package com.fundamentalandroid.dicodingevents.ui.upcoming
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class UpcomingViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
